b832428f0db58447d6feacf53bad74aef0e7b549
Jakub Talich <128613548+jakubtalich@users.noreply.github.com>
2024-10-04 01:22:07 +0200

    Fixed printhostname.nim

--- modules/printhostname.nim
+++ modules/printhostname.nim
@@ -1,18 +1,17 @@
 import options
 
-when defined(freebsd):
+when defined(freebsd): # This is for FreeBSD (tested on 14.1)
   const HOST_NAME_MAX: csize_t = 255
   proc getHostname(name: cstring, len: csize_t): int32 {.cdecl, importc: "gethostname", header: "<unistd.h>".}
-else:
-  when defined(solaris):
-    const HOST_NAME_MAX: csize_t = 255
-  else:
-    let HOST_NAME_MAX {.importc: "HOST_NAME_MAX", header: "<unistd.h>".}: csize_t
+elif defined(solaris): # This is for Solaris/Illumos (tested on OmniOS r151050 and Solaris 11.4 CBE)
+  const HOST_NAME_MAX: csize_t = 255
+else: # This is for POSIX - Linux (tested on Fedora 40)
+  let HOST_NAME_MAX {.importc: "HOST_NAME_MAX", header: "<unistd.h>".}: csize_t
   proc getHostname(name: cstring, len: csize_t): int32 {.cdecl, importc: "gethostname", header: "<unistd.h>".}
 
 proc printHostname*(): Option[TaintedString] =
   runnableExamples:
-    import hostname, options
+    import printhostname, options
     let hostname1 = hostname().get
     let hostname2 = hostname().get(otherwise = TaintedString("example.com"))
 
